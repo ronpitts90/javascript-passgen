5901dc6b50f126ca57a2a7bb8cfc9f7ed5aee9f7
Ronald Pitts <ronpitts81@gmail.com>
2020-06-28 15:13:21 -0400

    commiting hw files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # javascript-passgen
 JS HW password generator
+1-Create a index.html, style.css, and script.js file
+2-Link my JS file and CSS file to HTML
+3-Copy Bootstrap link
+4-copy bootsrap card code
+5-Go to JS file and define varibales/ criteria for the password such as lowercase, uppercase, etc
+6-create functions for generating a password
+7-define the varibale for password length
+8-create if loops for different criteria selection
+9-grab element id's from the password and generate sections of the card body
+10-make sure all links are functioning, test, and push 
--- script.js
+++ script.js
@@ -1,40 +1,51 @@
- 
 
-// generate pass
-function generate(){
 
-//set password lenght/complexity
- let complexity = document.getElementById("slider").values;
-
-//possibel password values 
-values="QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()+";
-
-let password ="";
-
-// create for loop
-for(var i=8; i<=complexity;i++){
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-}
+var lowercasealphabets=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var Uppercasealphabets=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
+var numbers=["0","1","2","3","4","5","6","7","8","9"];
+var special = [ '!','"', '#','$', '%','&','(',')','*','+','-',"'",'.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~']; 
 
 
 
-// add passwod to textbox area
-document.getElementById("display").value= password;
-
-// add to previous passwords
-document.getElementById("lastpassword").innerHTML += "<br/>";
-
-}
-
-// set default length to 
-document.getElementById("length").innerHTML="length: 20";
-
-// function for tapered length
-document.getElementById("slider").oninput= function(){
-    if (document.getElementById("slider").value > 0){
-        document.getElementById("lenght").innerHTML = "Length:" + document.getElementById("slider").value; 
+function generaterandomPassword() {
+    var newCharacterlist=[];
+    var verifyLowerCase=confirm("Would you like to incude Lower Case letters in your password (click OK for YES and Cancel for NO)");
+    var verifyUpperCase=confirm("would you like to include Upper Case letters in your password (click OK for YES and Cancel for NO)");
+    var verifyNumbers=confirm("would you like to include numbers in your password (click OK for YES and Cancel for NO)");
+    var verifySpecial=confirm("would you like to include special characters in your passwords (!@#$ (click OK for YES and Cancel for NO)");
+    while (verifyLowerCase == false && verifyUpperCase == false && verifyNumbers == false && verifySpecial == false) {
+        alert("You must pick atleast one type");
+        verifyLowerCase=confirm("Would you like to incude Lower Case letters in your password (click OK for YES and Cancel for NO)");
+        verifyUpperCase=confirm("would you like to include Upper Case letters in your password (click OK for YES and Cancel for NO)");
+        verifyNumbers=confirm("would you like to include numbers in your password (click OK for YES and Cancel for NO)");
+        verifySpecial=confirm("would you like to include special characters in your passwords (!@#$ (click OK for YES and Cancel for NO)");
     }
-    else {
-        document.getElementById("length").innerHTML = "Length: 1";
+
+
+    var choosepasswordLength=parseInt(prompt("Please pick the characters you like and make sure they are between 8 and 128"));
+    while(choosepasswordLength < 8 || choosepasswordLength > 128) {
+        alert("Please choose a number between 8 and 128");
+        choosepasswordLength=parseInt(prompt("Please pick the characters you like and make sure they are between 8 and 128"));
+    } 
+    if (verifyLowerCase){
+        newCharacterlist = newCharacterlist.concat(lowercasealphabets);
     }
+    if (verifyUpperCase){
+        newCharacterlist = newCharacterlist.concat(Uppercasealphabets);
+    }
+    if (verifyNumbers){
+        newCharacterlist = newCharacterlist.concat(numbers);
+    }
+    if (verifySpecial){
+        newCharacterlist = newCharacterlist.concat(special);
+    }
+    var passwordString=""
+    for (var i = 0; i < choosepasswordLength; i++) {
+        passwordString = passwordString + newCharacterlist[Math.floor(Math.random() * newCharacterlist.length)];
+    }
+    passwordEntry.textContent=passwordString;    
 }
+
+var generateButton= document.getElementById("generate");
+var passwordEntry= document.getElementById("password");
+generateButton.onclick = generaterandomPassword;
